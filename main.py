711492073d5023d495f981793882b2f1f92397a9
Janan Patel <jananpatel2002@gmail.com>
2023-01-21 15:39:06 -0500

    test commit

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from workouts import workouts
+from workouts import workouts_dict  # imports the dictionary (workouts) from workouts.py
 
-for workout in workouts['arms']:
-    print(workout)
+sName = input("What is your name?").title()
 
-    # For loop that will loop through whatever workout and gives you each workout
+try:
+    sChoice = input("Would you like to workout today? ").lower()  # Gets the input for the yes or no
+    if sChoice == 'yes':
+        for category in workouts_dict:
+            print(category.title())
+        category = input("What workout category would you like to do? ").lower()
+        exercises = workouts_dict[category]
+
+except KeyError:
+    print("Workouts for this section is unavailable for our application ")
+
+else:
+    for exercise in exercises:
+        print(exercise)
+
+# if sChoice != " Yes" and sChoice != " yes" and sChoice != " No" and sChoice != " no":
+# print("Enter yes or no to continue.")
+# exit()
--- workouts.py
+++ workouts.py
@@ -1,5 +1,10 @@
-workouts = {
-    'arms': ['workout 1', 'workout 2'],
-    'legs': [],
-    'chest': []
+workouts_dict = {
+    'arms': ['4x10 Hammer Curls', '4x10 Bicep Curls', '4x10 Tricep Pushdowns', '4x10 Tricep Extensions',
+             '4X10 Skullcrushers'],
+    'legs': ['4X5 Barbell Squats', '4x10 Romanian Deadlifts', '4X10 Quad Extensions', '4x10 Calf Raises',
+             '4x10 Leg Press'],
+    'chest': ['4x10 Flat Dumbbell press', '4x10 Incline Dumbbell Press', '4x10 Machine Chest Fly', '4X10 Dips',
+              '4x10 high to low cable flies'],
+    'back': ['4x10 Lat Pulldowns', '4x10 Seated Rows', '4x10 Rope Pullovers', '4x10 Dumbbell Shrugs', '4X10 Tbar Rows'],
+    'shoulders': ['4x10 Dumbbell Shoulder Press', '4x10 Dumbbell Lateral Raise', '4x10 Rear Delt flies', '4x10 Facepulls', '4x10 Cable Lateral Raise'],
 }
