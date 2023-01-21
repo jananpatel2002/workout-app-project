d2302b271db8f96e069f33d7b8ffc11c204c3d0c
Janan Patel <jananpatel2002@gmail.com>
2023-01-21 16:45:45 -0500

    added gui.py file

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from workouts import workouts_dict  # imports the dictionary (workouts) from workouts.py
 
-sName = input("What is your name?").title()
+workout_str = ""
+
+print("Hello! Welcome to Gym Bro! This is a program designed to help you find a workout routine. \n")
+name = input("What is your name? ").title()
 
 try:
-    sChoice = input("Would you like to workout today? ").lower()  # Gets the input for the yes or no
-    if sChoice == 'yes':
-        for category in workouts_dict:
-            print(category.title())
-        category = input("What workout category would you like to do? ").lower()
+    choice = input("\nWould you like to workout today? ").lower()  # Gets the input for the yes or no
+    if choice == 'yes':
+
+        for exercise_type in workouts_dict:
+            workout_str += exercise_type.title() + ", "
+        workout_str = workout_str[:-2]
+        print(f"\n{workout_str}")
+        category = input(f"Welcome {name}! What workout category would you like to do from the list above? ").lower()
         exercises = workouts_dict[category]
+    else:
+        print("Have a nice day!")
+        exit()
 
 except KeyError:
     print("Workouts for this section is unavailable for our application ")
 
 else:
     for exercise in exercises:
-        print(exercise)
+        print(f"\n{exercise}")
 
 # if sChoice != " Yes" and sChoice != " yes" and sChoice != " No" and sChoice != " no":
 # print("Enter yes or no to continue.")
--- workouts.py
+++ workouts.py
@@ -6,5 +6,17 @@ workouts_dict = {
     'chest': ['4x10 Flat Dumbbell press', '4x10 Incline Dumbbell Press', '4x10 Machine Chest Fly', '4X10 Dips',
               '4x10 high to low cable flies'],
     'back': ['4x10 Lat Pulldowns', '4x10 Seated Rows', '4x10 Rope Pullovers', '4x10 Dumbbell Shrugs', '4X10 Tbar Rows'],
-    'shoulders': ['4x10 Dumbbell Shoulder Press', '4x10 Dumbbell Lateral Raise', '4x10 Rear Delt flies', '4x10 Facepulls', '4x10 Cable Lateral Raise'],
+    'shoulders': ['4x10 Dumbbell Shoulder Press', '4x10 Dumbbell Lateral Raise', '4x10 Rear Delt flies',
+                  '4x10 Facepulls', '4x10 Cable Lateral Raise'],
+    'biceps': ['4x10 Hammer Curls', '4x10 Bicep Curls', '4x10 Reverse Curls'],
+    'triceps': ['4x10 Tricep Pushdowns', '4x10 tricep extensions', '4x10 Skullcrushers'],
+    'push': ['4X10 dumbbell flatpress', '4x10 incline dumbbell press', '4x10 machine chest flies',
+             '4x10 Tricep Extensions', '4x10 Tricep Pushdowns', '4X10 Dumbbell Lateral Raises'],
+    'pull': ['4x10 Lat Pulldowns', '4x10 seated rows', '4x10 Rope Pullovers', '4x10 Dumbbell Shrugs',
+             '4x10 Hammer Curls', '4x10 Bicep Curls'],
+    'chest and back': ['4x10 Dumbbell Flat press', '4X10 Barbell Rows', '4X10 Lat Pulldowns', '4X10 Dips',
+                       '4X10 Chest flies', '4X10 Rope Pullovers'],
+    'arms and shoulders': ['4x10 Lateral Raises', ]
+    'core': ['4x20 crunches', '4x20 sit ups', '4x10 Leg raises', '1 minute plank'],
+
 }
